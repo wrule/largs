c6bad3f6e4b26feb257e218958133159dc0c19c5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-13 17:32:40 +0800

    一些测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,9 @@
 import * as la from '.';
 
 function main() {
-  console.log(la.parse(process.argv));
+  console.log(process.argv);
+  console.log(la.file_name(process.argv[1]));
+  console.log(la.unparse_str(la.parse_str(`yarn dev --num 123 -b true1 ---u = NaN -a null -----a`)));
 }
 
 main();
